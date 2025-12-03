1a74dd6ab137f7a01a2546f309371e62b32a7b74
musfiqur-rahman1218 <md.musfiqur.rahman@smu.ca>
2025-12-03 02:50:16 -0400

    Add cors origin

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,10 +137,13 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 extra_cors = os.getenv("CORS_ALLOWED_ORIGINS")
 if extra_cors:
